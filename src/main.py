6149011b6816ce054449072b801147656650af20
Yasuhiro WATANABE <info@nabeyasu.com>
2024-12-30 16:13:34 +0900

    Add initial project structure with README, .gitignore, and sample code

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from modules import data_struct
+
+def main():
+    print("Hello World!")
+
+    data_struct.practice_function()
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/modules/data_struct.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+def practice_function():
+    print("This is a practice function.")
+
+    
+
+
+
+if __name__ == "__main__":
+    practice_function()
