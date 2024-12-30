e86585fe767561a728baf58301540529ff009fc6
Yasuhiro WATANABE <info@nabeyasu.com>
2024-12-30 21:31:31 +0900

    Add std_lib module and update main function to run std_lib

---
+++ src/modules/std_lib.py
@@ -0,0 +1,25 @@
+
+
+
+def run():
+    print("std_lib.py")
+
+    # practice_02()
+    # practice_06()
+    # practice_07()
+    # practice_08()
+    # practice_10()
+    # practice_11()
+    # practice_12()
+    # practice_13()
+    # practice_15()
+    # practice_17()
+    # practice_18()
+    # practice_19()
+    # practice_20()
+    # practice_22()
+    # practice_23()
+    # practice_24()
+
+if __name__ == "__main__":
+    run()
