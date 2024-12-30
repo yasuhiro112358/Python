36e6d62c50e99a6d37fdac30bafbbbada81f8b3e
Yasuhiro WATANABE <info@nabeyasu.com>
2024-12-30 21:55:07 +0900

    Add practice functions for shuffling names and sampling without duplicates in std_lib

--- src/modules/std_lib.py
+++ src/modules/std_lib.py
@@ -1,5 +1,6 @@
 import random
 
+
 def practice_02():
     print("practice_02")
 
@@ -9,11 +10,40 @@ def practice_02():
     n = random.randint(1, 6) # 1 <= n <= 6
     print(n)
 
+def practice_03():
+    print("practice_03")
+
+    name = ["Taro", "Jiro", "Saburo", "Shiro", "Goro"]
+    random.shuffle(name) # 破壊的メソッド
+    print(name)
+
+    winner = random.choice(name)
+    print(winner)
+
+    # winners = random.choices(name, k=3) # 重複あり
+    winners = random.sample(name, k=3) # 重複なし
+    print(winners)
+
+    names = ["Taro", "Taro", "Taro", "Taro", "Taro", "Taro", "Taro", "Taro", "Taro", "Taro", "Taro", "Taro", "Jiro", "Saburo", "Shiro", "Goro"]
+    names = list(set(names)) # 重複削除 => setにした段階で重複が削除される
+    print(names)
+    winners = random.sample(name, k=3) # 重複なし
+    print(winners)
+
+def practice_04():
+    print("practice_04")
+
+
+
+
 
 def run():
     print("std_lib.py")
 
-    practice_02()
+    # practice_02()
+    # practice_03()
+    practice_04()
+
     # practice_06()
     # practice_07()
     # practice_08()
