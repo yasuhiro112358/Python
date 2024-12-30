04e6ccffc426036f1b4e6ef62df11b4cfa51a46a
Yasuhiro WATANABE <info@nabeyasu.com>
2024-12-30 21:42:03 +0900

    Add practice function for random number generation in std_lib

--- src/modules/std_lib.py
+++ src/modules/std_lib.py
@@ -1,10 +1,19 @@
+import random
 
+def practice_02():
+    print("practice_02")
+
+    n = random.random() # 0.0 <= n < 1.0
+    print(n)
+
+    n = random.randint(1, 6) # 1 <= n <= 6
+    print(n)
 
 
 def run():
     print("std_lib.py")
 
-    # practice_02()
+    practice_02()
     # practice_06()
     # practice_07()
     # practice_08()
