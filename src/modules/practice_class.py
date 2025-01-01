8c3fa2444a6996236bcff752eb78904c991837ad
Yasuhiro WATANABE <info@nabeyasu.com>
2025-01-01 20:08:01 +0900

    Add practice function to demonstrate usage of pprint for displaying post details

--- src/modules/practice_class.py
+++ src/modules/practice_class.py
@@ -1,7 +1,23 @@
+from pprint import pprint
+
 def practice_02():
     print("practice_02")
 
+    def show(post):
+        print(f"Title: {post['title']} | Content: {post['content']} | {post['likes']} like(s)")
+        print()
+
+    posts = [
+        {"title": "First post", "content": "Hello World!", "likes": 9},
+        {"title": "Introduce", "content": "My name is Taro.", "likes": 10},
+        {"title": "Final post", "content": "Goodbye!", "likes": 7},
+    ]
+
+    pprint(posts)
     
+    show(posts[0])
+    show(posts[1])
+    show(posts[2])
 
 
 
